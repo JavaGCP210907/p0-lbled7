acf5b566d564d473299520119d47930f2d45b37b
lbled7 <lbled777@gmail.com>
2021-09-20 11:48:18 -0400

    customerDao adds customerList twice

--- src/main/java/com/revature/dao/CustomerDao.java
+++ src/main/java/com/revature/dao/CustomerDao.java
@@ -1,14 +1,50 @@
 package com.revature.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.revature.models.Customer;
+import com.revature.util.ConnectionUtil;
 
 public class CustomerDao implements CustomerDaoInterface {
 
 	@Override
 	public List<Customer> getCustomers() {
-		// TODO Auto-generated method stub
+		
+		try(Connection conn = ConnectionUtil.getConnection()){
+			
+			ResultSet rs = null;
+			
+			String sql = "select * from customers";
+			Statement s = conn.createStatement();
+			rs = s.executeQuery(sql);
+			
+			List<Customer> customerList = new ArrayList<>();
+			
+			while (rs.next()) {
+				
+				Customer c = new Customer();
+				c.setCustomer_id(rs.getInt(0));
+				c.setF_name(rs.getString(1));
+				c.setL_name(rs.getString(2));
+				c.setDebit_balance(rs.getDouble(3));
+				c.setCard_id(rs.getInt(4));
+				
+				customerList.add(c);
+			}
+			return customerList;
+		}
+		catch(SQLException e) {
+			System.out.println("Caught exception in getCustomer");
+			e.printStackTrace();
+		}
+		
+		
 		return null;
 	}
 
@@ -20,7 +56,40 @@ public class CustomerDao implements CustomerDaoInterface {
 
 	@Override
 	public List<Customer> getCustomerById(int id) {
-		// TODO Auto-generated method stub
+		
+		try(Connection conn = ConnectionUtil.getConnection()){
+			
+			ResultSet rs = null;
+			
+			String sql = "select * from customers where customer_id = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			rs = ps.executeQuery();
+			
+			List<Customer> customerList = new ArrayList<>();
+			while(rs.next()) {
+				Customer c = new Customer(
+						rs.getInt(0),
+						rs.getString(1),
+						rs.getString(2),
+						rs.getDouble(3),
+						rs.getInt(4)
+						);
+				
+				customerList.add(c);
+			}
+			return customerList;
+			
+		}
+		catch(SQLException e) {
+			System.out.println("Problem with connection");
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
 		return null;
 	}
 
